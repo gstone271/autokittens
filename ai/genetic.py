169752a6c5c3249aff01ff6be7bb876ac066e3bb
Baris Ege Inan <barisegeinan@gmail.com>
2018-11-24 15:43:52 -0800

    Updated program for kittens game
    Wrote elementary fitness function
    Changed printInfo and randomGenome functions
    Removed unused stuff

--- ai/genetic.py
+++ ai/genetic.py
@@ -15,9 +15,9 @@ class GeneralizedBeamSearch:
             newGen[pos] = self.mutate1(newGen[pos])
         return (alwaysChange, newGen)
 
-    def breed(self, p1, p2, problem, pMut):
+    def breed(self, p1, p2, pMut):
         crossoverIdx = random.randrange(0, len(p1))
-        (c1, c2) = tuple([ self.score(self.mutate(self.cross(a, b, crossoverIdx), pMut, mustMutate), problem) for (a, b, mustMutate) in [(p1, p2, True), (p2, p1, False)]])
+        (c1, c2) = tuple([ self.score(self.mutate(self.cross(a, b, crossoverIdx), pMut, mustMutate)) for (a, b, mustMutate) in [(p1, p2, True), (p2, p1, False)]])
         return (c1, c2)
 
     def sortGeneration(self, pop, T):
@@ -38,7 +38,7 @@ class GeneralizedBeamSearch:
                     best[i] = (fitness, True, gen)
         return sorted(best, key=lambda s: s[0] if s[1] else -1, reverse=True)
 
-    def newGeneration(self, scored, problem, pMut, breedingPortion, mutatingPortion):
+    def newGeneration(self, scored, pMut, breedingPortion, mutatingPortion):
         numPairs = int(len(scored)*breedingPortion)
         toBreed = numPairs * 2
         toMutate = int(len(scored)*mutatingPortion)
@@ -46,8 +46,9 @@ class GeneralizedBeamSearch:
         breeders = [ gen for (fitness, fresh, gen) in scored[:toBreed]]
         random.shuffle(breeders)
         pairs = zip(breeders[:numPairs], breeders[numPairs:])
-        children = [ child for (p1, p2) in pairs for child in self.breed(p1, p2, problem, pMut)]
-        mutated = [ self.score(self.mutate(gen, pMut, True), problem) for (fitness, fresh, gen) in scored[:toMutate] ]
+        # Might want to pull score out of breed to simplify parallelization
+        children = [ child for (p1, p2) in pairs for child in self.breed(p1, p2, pMut)]
+        mutated = [ self.score(self.mutate(gen, pMut, True)) for (fitness, fresh, gen) in scored[:toMutate] ]
         keepers = [ (fitness, False, gen) for (fitness, fresh, gen) in scored[:toKeep] ]
         return keepers + mutated + children
 
@@ -55,7 +56,7 @@ class GeneralizedBeamSearch:
     # Mutation Schedule: Defines odds of mutating an individual gene, over time
     # Breeding portion: Defines number of breeding pairs per generation. Eg portion=1/3 means 2/3 of the populatin will be included in a breeding pair. Set to 0 for SA or HC.
     # Mutating portion: Defines portion of population to mutate (without breeding) each generation. Set to 0 for pure GA.
-    def run(self, problem, population, temperatureSchedule, mutationSchedule, breedingPortion, mutatingPortion, iterations, printProgress):
+    def run(self, population, temperatureSchedule, mutationSchedule, breedingPortion, mutatingPortion, iterations, printProgress):
         printed = 0
         for i in range(iterations):
             T = temperatureSchedule(i, iterations)
@@ -67,118 +68,102 @@ class GeneralizedBeamSearch:
                 print(f"Generation {i}: {scores}")
                 if (len(scored[0][2]) < 50):
                     print(scored[0][2])
-            population = self.newGeneration(scored, problem, pMut, breedingPortion, mutatingPortion)
+            population = self.newGeneration(scored, pMut, breedingPortion, mutatingPortion)
         scored = self.sortGeneration(population, 0)
         if (printProgress):
             print("Value:", scored[0][0])
-            self.printInfo(scored[0][2], problem)
+            self.printInfo(scored[0][2])
             print(scored[0][2])
         return scored[0][0]
 
-    def printInfo(self, gen, problem): pass
+    def printInfo(self, gen): pass
 
 # Stuff specific to Knapsack Problem. Should be removed later, but useful as code template
-class KnapsackProblem(GeneralizedBeamSearch):
+class KittensProblem(GeneralizedBeamSearch):
+    def __init__(self, buildings, build_order_length):
+        self.buildings = buildings
+        self.build_order_length = build_order_length
+
     # score is the function that should send the genome to another computer to be tested for fitness in Simba
     # we might want to keep a cache of recently-scored genomes, and use the cached result if we have duplicate genomes in the population, since scoring is extremely expensive
-    def score(self, mut, problem):
-        (objs, cap) = problem
+    def score(self, mut):
         (fresh, gen) = mut
-        if (self.overWeight(gen, objs, cap)):
-            return (0, fresh, gen)
-        return (sum([ take*value for take, (weight, value) in zip(gen, objs) ]), fresh, gen)
+        return (self.fitness_function(gen), fresh, gen)
 
+    # TODO: this should simulate the game and compute a score. The current implementation is a stub for testing.
+    def fitness_function(self, gen):
+        fitness = 0
+        if gen[0] == "Field":
+            fitness += 1
+        if gen[1] == "Hut":
+            fitness += 1
+        if gen[2] == "Barn":
+            fitness += 1
+        if gen[3] == "Library":
+            fitness += 1
+        return fitness
+
+    # Changes a single element of the build order
     def mutate1(self, gene):
-        return gene ^ 1
+        return random.choice(self.buildings)
 
-    def printInfo(self, gen, problem):
-        print("Weight:", self.totalWeight(gen, problem[0]))
+    def printInfo(self, gen):
+        print(",".join(gen[0:min(10, len(gen))]))
 
-    def totalWeight(self, gen, objs): 
-        return sum([ take*weight for take, (weight, value) in zip(gen, objs) ])
-
-    def overWeight(self, gen, objs, cap): 
-        for take, (weight, value) in zip(gen, objs):
-            if (take):
-                cap -= weight
-                if (cap < 0):
-                    return True
-        return False
-
-    def randomObj(self, capacity):
-        return (random.randrange(1, capacity), random.randrange(1, capacity))
-
-    def randomGenome(self, objs, cap):
-        numTaken = numpy.random.poisson(2)
-        gen = [0] * len(objs)
-        for pos in random.sample(range(len(objs)), numTaken):
-            gen[pos] = 1
-        return self.score((True, gen), (objs, cap))
-
-class ImprovedKnapsackProblem(KnapsackProblem):
-    def score(self, mut, problem):
-        (objs, cap) = problem
-        (fresh, gen) = mut
-        totalValue = 0
-        for take, (weight, value) in zip(gen, objs):
-            if (take):
-                cap -= weight
-                if (cap < 0):
-                    break
-                totalValue += value
-        return (totalValue, fresh, gen)
+    def randomGenome(self):
+        gen = random.choices(self.buildings, k=self.build_order_length) #Returns a list of k-size elements
+        return self.score((True, gen)) #Returns the score that corresponds to that list of elements
 
 # Code to find better parameters to GA/SA/HC. May be removed later, but useful as code template
-class MetaBeamSearch(GeneralizedBeamSearch):
-    def __init__(self, subSearch, subIterations):
-        self.subSearch = subSearch
-        self.subIterations = subIterations
-
-    def sigmoid(self, gene):
-        return 1 / (1 + math.exp(-gene))
-
-    def runSubTrial(self, instance, gen):
-        (logBaseT, sigTExp, tLinear, logBaseMut, sigMutExp, mutLinear, sigBreedingPortion, sigMutatingPortion) = gen
-        baseT = math.exp(logBaseT)
-        tExp = self.sigmoid(sigTExp) * 2
-        baseMut = math.exp(logBaseMut)
-        mutExp = self.sigmoid(sigMutExp) * 2
-        def temperatureSchedule(t, iterations):
-            try:
-                return max(baseT * pow(tExp, t) * (1 + tLinear * t / iterations), 0)
-            except OverflowError:
-                return math.inf
-        def mutationSchedule(t, iterations):
-            try:
-                return max(min(baseMut * pow(mutExp, t) * (1 + mutLinear * t / iterations), 1), 0)
-            except OverflowError:
-                return 1
-        breedingPortion = self.sigmoid(sigBreedingPortion) / 2
-        mutatingPortion = self.sigmoid(sigMutatingPortion) * (1 - breedingPortion * 2)
-        (subProblem, subPop) = instance
-        return self.subSearch.run(subProblem, subPop, temperatureSchedule, mutationSchedule, breedingPortion, mutatingPortion, self.subIterations, False)
-
-    def score(self, mut, problem):
-        (fresh, gen) = mut
-        #todo def schedules based on float params
-        with multiprocessing.Pool(24) as p:
-            return (sum(p.starmap(self.runSubTrial, zip(problem, [gen] * len(problem)))) / len(problem), fresh, gen)
-
-    def mutate1(self, gene):
-        return (gene + random.random() - .5) * (random.random() + .5)
-
-    #todo generalize capacity--include data in these objects
-    def randomObj(self, capacity, numObjects, populationSize):
-        objs = [ self.subSearch.randomObj(capacity) for i in range(numObjects) ]
-        population = [ self.subSearch.randomGenome(objs, capacity) for i in range(populationSize) ]
-        return ((objs, capacity), population)
-
-    def randomGenome(self, problem):
-        gen = [ (random.random() - .5) * 4 for i in range(8) ]
-        return self.score((True, gen), problem)
+# class MetaBeamSearch(GeneralizedBeamSearch):
+#     def __init__(self, subSearch, subIterations):
+#         self.subSearch = subSearch
+#         self.subIterations = subIterations
+#
+#     def sigmoid(self, gene):
+#         return 1 / (1 + math.exp(-gene))
+#
+#     def runSubTrial(self, instance, gen):
+#         (logBaseT, sigTExp, tLinear, logBaseMut, sigMutExp, mutLinear, sigBreedingPortion, sigMutatingPortion) = gen
+#         baseT = math.exp(logBaseT)
+#         tExp = self.sigmoid(sigTExp) * 2
+#         baseMut = math.exp(logBaseMut)
+#         mutExp = self.sigmoid(sigMutExp) * 2
+#         def temperatureSchedule(t, iterations):
+#             try:
+#                 return max(baseT * pow(tExp, t) * (1 + tLinear * t / iterations), 0)
+#             except OverflowError:
+#                 return math.inf
+#         def mutationSchedule(t, iterations):
+#             try:
+#                 return max(min(baseMut * pow(mutExp, t) * (1 + mutLinear * t / iterations), 1), 0)
+#             except OverflowError:
+#                 return 1
+#         breedingPortion = self.sigmoid(sigBreedingPortion) / 2
+#         mutatingPortion = self.sigmoid(sigMutatingPortion) * (1 - breedingPortion * 2)
+#         (subProblem, subPop) = instance
+#         return self.subSearch.run(subProblem, subPop, temperatureSchedule, mutationSchedule, breedingPortion, mutatingPortion, self.subIterations, False)
+#
+#     def score(self, mut, problem):
+#         (fresh, gen) = mut
+#         #todo def schedules based on float params
+#         with multiprocessing.Pool(24) as p:
+#             return (sum(p.starmap(self.runSubTrial, zip(problem, [gen] * len(problem)))) / len(problem), fresh, gen)
+#
+#     def mutate1(self, gene):
+#         return (gene + random.random() - .5) * (random.random() + .5)
+#
+#     #todo generalize capacity--include data in these objects
+#     def randomObj(self, capacity, numObjects, populationSize):
+#         objs = [ self.subSearch.randomObj(capacity) for i in range(numObjects) ]
+#         population = [ self.subSearch.randomGenome((objs, capacity)) for i in range(populationSize) ]
+#         return ((objs, capacity), population)
+#
+#     def randomGenome(self, problem):
+#         gen = [ (random.random() - .5) * 4 for i in range(8) ]
+#         return self.score((True, gen), problem)
 
 #example parameters
-capacity = 1000
 def temperatureSchedule2(t, iterations):
     #return max((1 - t / iterations * 1.2) * 10000, 0)
     return pow(.98, (t)) * 10000 * max((1 - t / iterations * 1.2), 0)
@@ -190,34 +175,20 @@ def mutationSchedule(t, iterations):
     return 0.004
 
 #define and run different problems
-knapsack = KnapsackProblem()
-def knapsackTrial(j):
-    numObjects = 20
+def kittensTrial(j):
+    buildings = [
+        "Field",
+        "Hut",
+        "Barn",
+        "Pasture",
+        "Library",
+        "Mine"
+    ]
+    build_order_length = 100
+    kittensProblem = KittensProblem(buildings, build_order_length)
     populationSize = 800
-    objs = [ knapsack.randomObj(capacity) for i in range(numObjects) ]
-    population = [ knapsack.randomGenome(objs, capacity) for i in range(populationSize) ]
-    if (numObjects < 50):
-        print("Objects:", objs)
-    return knapsack.run((objs, capacity), population, temperatureSchedule0, mutationSchedule, 1/3, 0/2, 1000, j == 0)
+    population = [ kittensProblem.randomGenome() for i in range(populationSize) ]
+    return kittensProblem.run(population, temperatureSchedule0, mutationSchedule, 1/3, 0/2, 10, j == 0)
 
-metaSearch = MetaBeamSearch(ImprovedKnapsackProblem(), 1000)
-def metaTrial(j):
-    numObjects = 24
-    populationSize = 80
-    problem = [ metaSearch.randomObj(capacity, 80, 150) for i in range(numObjects) ]
-    population = [ metaSearch.randomGenome(problem) for i in range(populationSize) ]
-    return metaSearch.run(problem, population, temperatureSchedule0 , mutationSchedule, 1/3, 0/2, 500, True)
-
-def averageKnapsackTrial(trials):
-    with multiprocessing.Pool(8) as p:
-        results = p.map(knapsackTrial, range(trials))
-        print(results)
-        print("Average: ", sum(results)/trials)
-
-#problem = [ metaSearch.randomObj(capacity, 80, 150) for i in range(24) ]
-#print(metaSearch.score((True, [-0.6054259396832666, -1.2123086049650311, -0.4978623552545826, 0.29816281468476546, -7.81723492065458, -1.041818686799827, 0.17616663713567887, 1.1142006038490322]), problem))
-#print(metaSearch.score((True, [-99, -99, -99, -7, 0, 0, 0.8, -99]), problem))
-#metaTrial(0)
-
-knapsackTrial(0)
-#averageKnapsackTrial(8)
+if __name__ == "__main__":
+    kittensTrial(0)
