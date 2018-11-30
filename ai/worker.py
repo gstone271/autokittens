be7b7f03976efca66fe3080be12a37fd365c3dbf
Baris Ege Inan <barisegeinan@gmail.com>
2018-11-29 18:51:30 -0800

    Added an import for TimeoutException

--- ai/worker.py
+++ ai/worker.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 def run_browser(baseDir, simbaSettings): #Runs the chrome browser
 	with open(os.path.join(baseDir, "bot.js")) as f:
