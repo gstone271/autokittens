e269b4a039ce96a682c2ddef7e2eab90227ffe47
KH <kaharvey@pdx.edu>
2018-11-30 10:18:24 -0800

    Added work done in lab

--- ai/captain.py
+++ ai/captain.py
@@ -1,8 +1,10 @@
-# The 'master' program that generates genomes for multiple
-# scotty programs to run simba with. 
-# Inputs: list of genomes, list of computers, (N run times?)
+# The parallelised 'master' in the master/worker relationship for
+# fitness scoring/ running simba. 
+# Splits genomeList over computerList, and gives each computer
+# a scotty process with many genomes to run simba with. 
+# Inputs: list of genomes, list of computers
 # Ouputs: list of updated data/ scores
-# Operations: start scottys (2/computer?)
+# Operations: start scottys
 #             divide the genomes evenly among the computers and send each its chunk
 #             combine the results into a list. 
 #             (update genomes?)
@@ -10,9 +12,37 @@
 # One "leader" program being run has the GA and runs the workers, which run Simba
 # Author: Kallen Harvey
 # Sources noted where used.
-import python-daemon; # for parallelization of scottys across multiple computers?
-# possibly need to use bash script to start ssh to
-# each computer?
+from multiprocessing import Pool
+from scotty import startScotty
 
 #from genetic import (genome class/where genome info is stored)
+#list node has 1 computer (string of name/address) and many genomes 
+class Captain:
+    def start(self, computerList, genomeList):
+        computers = len(computerList)
+        genomes = len(genomeList)
+        if (computers == 0):
+            print("Computer List empty")
+            return
+        if (genomes == 0):
+            print("Genome List empty")
+            return
 
+        remain = genomes % computers
+        loops = genomes / computers
+        splitGenomes = list()
+        for x in range(computers):
+            thisCompsGenomes = list()
+            thisCompsName = computerList[x]
+            for i in range(loops):
+                #from 0 to loops-1, load genomeList[0] into new list for computer[x]
+                thisCompsGenomes.append(genomeList[0])
+                genomeList.pop(0)
+                if (remain != 0):
+                    thisCompsGenomes.append(genomeList[0])
+                    genomeList.pop(0)
+                    remain = remain - 1
+            splitGenomes.append((thisCompsName, thisCompsGenomes))
+        with Pool(processes = computers) as pool:
+            pool.starmap(func = startScotty, iterable = splitGenomes)
+            #get results back, combine into list, return
--- ai/scotty.py
+++ ai/scotty.py
@@ -9,3 +9,11 @@
 # selenium starts Simba, baris will do
 # baris: get fitness from simba
 # me: get genome to each computer (captain)
+def startScotty(self, computerName, genomeList):
+    #ssh open computer
+
+    fitnessList = list()
+    #run simba with each genome
+    for x in genomeList:
+        fitness = startSimba(genomeList[x])
+        fitnessList.append((fitness, genomeList[x]))
