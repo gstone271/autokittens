4c9afc1d4e4d13cebfdb1eefb49ce6d61f751f5a
Griffin Stone <gstone@pdx.edu>
2018-11-30 22:41:32 -0800

    Add performance log

--- ai/captain.py
+++ ai/captain.py
@@ -104,7 +104,11 @@ def run(genomeList):
         except FileExistsError:
             pass
     computerList = getWorkingComputers(fullComputerList)
-    return start(computerList, genomeList)
+    generationStartTime = time.time()
+    result = start(computerList, genomeList)
+    timePassed = time.time() - generationStartTime
+    with open(f'./logs/{startTime}/timings.log', 'w') as f:
+        print(timePassed, file=f)
 
 if __name__ == '__main__':
     genomeList = exampleGenomes
