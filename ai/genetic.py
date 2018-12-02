a99c0ff4194941fe75baf3bc48d91120f13effd7
Griffin Stone <gstone@pdx.edu>
2018-12-01 18:27:01 -0800

    Decrease bot speed

--- ai/genetic.py
+++ ai/genetic.py
@@ -203,7 +203,7 @@ def toSimbaSettings(genome):
                 break
     queue = ",".join(blds)
     jobQueue = ",".join([f'"{job}"' for job in genome if job in jobsSet])
-    return f'{{"queue": [{queue}], "jobQueue": [{jobQueue}], "geneticAlgorithm": true, "speed": 4096, "disableTimeskip": true, "desiredTicksPerLoop": 16, "running": true, "autoHunt": true, "autoSteel": true}}'
+    return f'{{"queue": [{queue}], "jobQueue": [{jobQueue}], "geneticAlgorithm": true, "speed": 4096, "disableTimeskip": true, "desiredTicksPerLoop": 32, "running": true, "autoHunt": true, "autoSteel": true}}'
 
 buildings = [
   "Catnip field",
